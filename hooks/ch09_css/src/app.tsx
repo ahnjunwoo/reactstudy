b7800074c09c3e6422609edc7c6997ca7a39c087
안준우 <codegun@ogqcorp.com>
2020-03-28 17:42:23 +0900

    ddd

---
+++ hooks/ch09_css/src/app.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import SassComponent from './SassComponent';
+import {Component} from "react";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <SassComponent />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ hooks/ch09_css/webpack.config.js
@@ -0,0 +1,149 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require("webpack");
+const path = require("path");
+
+const basePath = __dirname;
+
+// style files regexes
+const cssRegex = /\.css$/;
+const cssModuleRegex = /\.module\.css$/;
+const sassRegex = /\.(scss|sass)$/;
+const sassModuleRegex = /\.module\.(scss|sass)$/;
+
+module.exports = {
+  context: path.join(basePath, "src"),
+  resolve: {
+    extensions: [".js", ".ts", ".tsx"]
+  },
+  entry: ["@babel/polyfill", "./index.tsx"],
+  output: {
+    path: path.join(basePath, "dist"),
+    filename: "bundle.js"
+  },
+  devtool: "source-map",
+  devServer: {
+    contentBase: "./dist", // Content base
+    inline: true, // Enable watch and live reload
+    host: "localhost",
+    port: 8080,
+    stats: "errors-only"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        loader: "awesome-typescript-loader",
+        options: {
+          useBabel: true,
+          babelCore: "@babel/core" // needed for Babel v7
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: "file-loader",
+        options: {
+          name: "assets/img/[name].[ext]?[hash]"
+        }
+      },
+      {
+        test: cssRegex,
+        exclude: cssModuleRegex,
+        use: getStyleLoaders({
+          importLoaders: 1,
+          sourceMap: isEnvProduction && shouldUseSourceMap,
+        }),
+        // Don't consider CSS imports dead code even if the
+        // containing package claims to have no side effects.
+        // Remove this when webpack adds a warning or an error for this.
+        // See https://github.com/webpack/webpack/issues/6571
+        sideEffects: true,
+      },
+      // Adds support for CSS Modules (https://github.com/css-modules/css-modules)
+      // using the extension .module.css
+      {
+        test: cssModuleRegex,
+        use: getStyleLoaders({
+          importLoaders: 1,
+          sourceMap: isEnvProduction && shouldUseSourceMap,
+          modules: {
+            getLocalIdent: getCSSModuleLocalIdent,
+          },
+        }),
+      },
+      // Opt-in support for SASS (using .scss or .sass extensions).
+      // By default we support SASS Modules with the
+      // extensions .module.scss or .module.sass
+      {
+        test: sassRegex,
+        exclude: sassModuleRegex,
+        use: getStyleLoaders({
+          importLoaders: 3,
+          sourceMap: isEnvProduction && shouldUseSourceMap
+        }).concat({
+          loader: require.resolve('sass-loader'),
+          options: {
+            sassOptions: {
+              includePaths: [paths.appSrc + '/styles']
+            },
+            sourceMap: isEnvProduction && shouldUseSourceMap,
+            prependData: `@import 'utils';`
+          }
+        }),
+        // Don't consider CSS imports dead code even if the
+        // containing package claims to have no side effects.
+        // Remove this when webpack adds a warning or an error for this.
+        // See https://github.com/webpack/webpack/issues/6571
+        sideEffects: true
+      },
+      // Adds support for CSS Modules, but using SASS
+      // using the extension .module.scss or .module.sass
+      {
+        test: sassModuleRegex,
+        use: getStyleLoaders(
+            {
+              importLoaders: 3,
+              sourceMap: isEnvProduction && shouldUseSourceMap,
+              modules: {
+                getLocalIdent: getCSSModuleLocalIdent,
+              },
+            },
+            'sass-loader'
+        ),
+      },
+      // "file" loader makes sure those assets get served by WebpackDevServer.
+      // When you `import` an asset, you get its (virtual) filename.
+      // In production, they would get copied to the `build` folder.
+      // This loader doesn't use a "test" so it will catch all modules
+      // that fall through the other loaders.
+      {
+        loader: require.resolve('file-loader'),
+        // Exclude `js` files to keep "css" loader working as it injects
+        // its runtime that would otherwise be processed through "file" loader.
+        // Also exclude `html` and `json` extensions so they get processed
+        // by webpacks internal loaders.
+        exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+        options: {
+          name: 'static/media/[name].[hash:8].[ext]',
+        },
+      }
+    ]
+  },
+  plugins: [
+    //Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
+    new HtmlWebpackPlugin({
+      filename: "index.html", //Name of file in ./dist/
+      template: "index.html", //Name of template in ./src
+      hash: true
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
+};
